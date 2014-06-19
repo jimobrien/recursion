3e2e7d759270fab967e1de1039c8c38baf644f31
Jim OBrien <support@thinkahead.com>
2014-06-19 15:06:08 -0500

    cleanup

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,10 +4,10 @@
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
   // your code goes here
-  //console.log(obj);
-  	var stringifier = new Stringifier();
 
-  	window.strif = Stringifier;
+	var stringifier = new Stringifier();
+
+	window.strif = Stringifier;
 
 	function Stringifier () {
 		this.stringified = '';
@@ -17,8 +17,6 @@ var stringifyJSON = function(obj) {
 	Stringifier.prototype.stringify = function (token) {
 		var type = typeof token;
 		this.token = token;
-
-		console.log(token);
 
 		if (token === null) {
 			this.stringifyNull();
@@ -97,72 +95,6 @@ var stringifyJSON = function(obj) {
 
 		this.stringified = open + inner + close;
 	};
-
-
-	// var Stringifier = {
-	// 	token: null,
-	// 	stringified: '',
-	// 	stringify: function (token) {
-	// 		var type = typeof token;
-	// 		this.token = token;
-
-	// 		if (token === null) {
-	// 			this.stringified += null;
-	// 		}
-
-	// 		if (type === 'boolean') {
-	// 			this.stringifyBool();
-	// 		}
-	// 		if (type === 'string') {
-	// 			this.stringifyStr();
-
-	// 		} else if (type === 'number') {
-	// 			this.stringifyNum();
-
-	// 		} else if (token instanceof Array) {
-	// 			this.stringifyArr();
-
-	// 		} else if (type === 'object') {
-	// 			this.stringifyObj();
-	// 		}
-
-			
-	// 		return this.stringified;
-
-	// 	},
-
-	// 	stringifyBool: function () {
-	// 		this.stringified += this.token;
-	// 	},
-
-	// 	stringifyStr: function () {
-	// 		this.stringified += "\"" + this.token + "\"";
-	// 	},
-
-	// 	stringifyNum: function () {
-	// 		this.stringified += this.token;
-			
-	// 	},
-
-	// 	stringifyArr: function () {
-	// 		var open = "[";
-	// 		var close = "]";
-	// 		var self = this;
-
-	// 		var inner = this.token.map(function (item) {
-	// 			return self.stringify(item);
-	// 		}).join();
-			
-	// 		console.log(this.stringified + '====' + inner);
-
-	// 		this.stringified = open + inner + close;
-	// 	},
-
-	// 	stringifyObj: function () {
-
-	// 	}
-
-	// };
 
 	return stringifier.stringify(obj);
   
